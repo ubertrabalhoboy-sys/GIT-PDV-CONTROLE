2f42ea8c625f0fdb19978b1cc7cc745f606b8394
ubertrabalhoboy-sys <ubertrabalhoboy@gmail.>
2025-09-17 16:23:57 -0300

    corrige api key

--- public/firebase-init.js
+++ public/firebase-init.js
@@ -8,13 +8,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 // --- ATENÇÃO ---
 // COPIE SUAS CHAVES DO ARQUIVO script.js E COLE AQUI DENTRO
 const firebaseConfig = {
-  piKey: "AIzaSyByZ1r41crqOadLXwHH2v9LgveyCkL6erE",
-        authDomain: "pdv-vendas-8a65a.firebaseapp.com",
-        projectId: "pdv-vendas-8a65a",
-        storageBucket: "pdv-vendas-8a65a.appspot.com",
-        messagingSenderId: "37533259212",
-        appId: "1:37533259212:web:9e79fecb52aa2b4765b969",
-        measurementId: "G-7PYWX52SEG"
+  apiKey: "AIzaSyByZ1r41crqOadLXwHH2v9LgveyCkL6erE",
+  authDomain: "pdv-vendas-8a65a.firebaseapp.com",
+  projectId: "pdv-vendas-8a65a",
+  storageBucket: "pdv-vendas-8a65a.firebasestorage.app",
+  messagingSenderId: "37533259212",
+  appId: "1:37533259212:web:9e79fecb52aa2b4765b969",
+  measurementId: "G-7PYWX52SEG"
 };
 
 // Inicializa o Firebase
